f2df20647a07d9d7392e3b1f0ef61ad7aa92ef24
Kaloyan D. Bozhkov <kaloyan@bozhkov.com>
2023-11-24 21:53:57 +0200

    replace import.meta with process.env

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -1,18 +1,14 @@
 /// <reference lib="dom" />
 
-// eslint-disable-next-line
-// @ts-nocheck
-const env = import.meta?.env ?? process?.env ?? {}
-
 export const getBaseUrl = (useRelativeOnFE = true) => {
   if (typeof window !== 'undefined' && useRelativeOnFE) return ''
 
-  if (env?.PUBLIC_DOMAIN && env?.NODE_ENV !== 'development')
-    return env?.PUBLIC_DOMAIN.includes('http')
-      ? env?.PUBLIC_DOMAIN
-      : `https://${env?.PUBLIC_DOMAIN}`
+  if (process.env?.PUBLIC_DOMAIN && process.env?.NODE_ENV !== 'development')
+    return process.env?.PUBLIC_DOMAIN.includes('http')
+      ? process.env?.PUBLIC_DOMAIN
+      : `https://${process.env?.PUBLIC_DOMAIN}`
 
-  return `http://localhost:${env?.VITE_PORT ?? 5173}`
+  return `http://localhost:${process.env?.VITE_PORT ?? 5173}`
 }
 
 export const formatDate = (d: Date) => `${d.getDate()}/${d.getMonth()}/${d.getFullYear()}`
