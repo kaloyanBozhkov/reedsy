5eec13f979cf04f8e93762cbc4cc2ccde29f022b
Kaloyan D. Bozhkov <kaloyan@bozhkov.com>
2023-11-24 20:12:01 +0200

    .

--- api/index.js
+++ api/index.js
@@ -1,3 +1,16 @@
-import app from '../dist/server/server/index'
+import fs from 'fs'
+import path from 'path'
 
+const parentDirectory = path.resolve(import.meta.dir, '..')
+
+fs.readdir(parentDirectory, (err, files) => {
+  if (err) {
+    console.error('Error reading parent directory:', err)
+    return
+  }
+
+  console.log('Files in the parent directory:', files)
+})
+
+import app from '../src/server/index'
 export default app
