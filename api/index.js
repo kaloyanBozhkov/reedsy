22637566f2387c7d4942c30fc54b2736152cb656
Kaloyan D. Bozhkov <kaloyan@bozhkov.com>
2023-11-24 20:17:21 +0200

    .

--- api/index.js
+++ api/index.js
@@ -1,6 +1,9 @@
 import fs from 'fs'
 import path from 'path'
+import { fileURLToPath } from 'url'
 
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 const parentDirectory = path.resolve(__dirname, '..')
 
 fs.readdir(parentDirectory, (err, files) => {
