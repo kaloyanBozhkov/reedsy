2592eb7ef18d659f56c6aeedf619b0d0809b34d7
Kaloyan D. Bozhkov <kaloyan@bozhkov.com>
2023-11-25 03:50:33 +0200

    .

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -3,18 +3,17 @@
 // eslint-disable-next-line
 // @ts-nocheck
 const env = typeof process === 'undefined' ? import.meta?.env : process.env
+const PUBLIC_DOMAIN = env?.PUBLIC_DOMAIN
+const NODE_ENV = env?.NODE_ENV ?? env?.MODE
+const PORT = env?.PORT ?? 3000
 
 export const getBaseUrl = (useRelativeOnFE = true) => {
   if (typeof window !== 'undefined' && useRelativeOnFE) return ''
 
-  if (env?.PUBLIC_DOMAIN && env?.NODE_ENV !== 'development')
-    return env?.PUBLIC_DOMAIN.includes('http')
-      ? env?.PUBLIC_DOMAIN
-      : `https://${env?.PUBLIC_DOMAIN}`
+  if (PUBLIC_DOMAIN && NODE_ENV !== 'development')
+    return env?.PUBLIC_DOMAIN.includes('http') ? PUBLIC_DOMAIN : `https://${PUBLIC_DOMAIN}`
 
-  if (env?.BASE_URL && env.PROD) return env?.BASE_URL
-
-  return `http://localhost:${env?.PORT ?? 3000}`
+  return `http://localhost:${PORT}`
 }
 
 export const formatDate = (d: Date) => `${d.getDate()}/${d.getMonth()}/${d.getFullYear()}`
