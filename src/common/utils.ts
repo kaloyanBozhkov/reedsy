aba7bc8768efbaeee0e4a7a296dec5960e1d7f7f
Kaloyan D. Bozhkov <kaloyan@bozhkov.com>
2023-11-25 04:54:28 +0200

    .

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -5,13 +5,12 @@
 const env = typeof process === 'undefined' ? import.meta?.env : process.env
 const PUBLIC_DOMAIN = env?.PUBLIC_DOMAIN ?? env?.VITE_PUBLIC_DOMAIN
 const ENV = env?.MODE ?? env?.NODE_ENV
+const isProd = ENV === 'production' || env?.PROD
 const PORT = env?.PORT ?? 3000
 
 export const getBaseUrl = (useRelativeOnFE = true) => {
   if (typeof window !== 'undefined' && useRelativeOnFE) return ''
-
-  if (ENV === 'development') return `http://localhost:${PORT}`
-
+  if (!isProd) return `http://localhost:${PORT}`
   if (PUBLIC_DOMAIN)
     return PUBLIC_DOMAIN.includes('http') ? PUBLIC_DOMAIN : `https://${PUBLIC_DOMAIN}`
 
