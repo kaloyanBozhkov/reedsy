061eb2e557512fe74522f21811f7538c10cb75f5
Kaloyan D. Bozhkov <kaloyan@bozhkov.com>
2023-11-25 04:14:32 +0200

    .

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -3,14 +3,14 @@
 // eslint-disable-next-line
 // @ts-nocheck
 const env = typeof process === 'undefined' ? import.meta?.env : process.env
-const PUBLIC_DOMAIN = env?.PUBLIC_DOMAIN
+const PUBLIC_DOMAIN = env?.PUBLIC_DOMAIN ?? env?.VITE_PUBLIC_DOMAIN
 const NODE_ENV = env?.NODE_ENV ?? env?.MODE
 const PORT = env?.PORT ?? 3000
 
 export const getBaseUrl = (useRelativeOnFE = true) => {
   if (typeof window !== 'undefined' && useRelativeOnFE) return ''
 
-  if (PUBLIC_DOMAIN && NODE_ENV !== 'development')
+  if (NODE_ENV !== 'development' && PUBLIC_DOMAIN)
     return env?.PUBLIC_DOMAIN.includes('http') ? PUBLIC_DOMAIN : `https://${PUBLIC_DOMAIN}`
 
   return `http://localhost:${PORT}`
